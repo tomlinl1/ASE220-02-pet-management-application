0976c8bc3133971a9df3937fb1db36126b984832
Joseph Ampfer <joseph.ampfer@gmail.com>
2025-02-17 20:53:02 -0500

    Fixed load more pets

--- js/main.js
+++ js/main.js
@@ -1,115 +1,112 @@
 let divRow = null;
 let currentIndex = 0;
-let itemsPerPage = 9;
-let original = 18;
-let total = 18;
+const itemsPerPage = 9;
+
 function loadPets() {
-  for(let i = currentIndex; i < Math.min(currentIndex + itemsPerPage, data.length); i++) {
+  let itemsLoaded = 0;
+  for (let i = currentIndex; i < Math.min(currentIndex + itemsPerPage, data.length); i++) {
+    itemsLoaded++;
+    if (i % 3 == 0) {
+      divRow = document.createElement("div");
+      divRow.classList.add("row");
+      document.querySelector("#all-pets").appendChild(divRow);
+    }
 
-      if (i % 3 == 0) {
-          divRow = document.createElement("div");
-          divRow.classList.add("row");
-          document.querySelector("#all-pets").appendChild(divRow);
-      }
+    let divCol = document.createElement("div");
+    divCol.classList.add("col");
 
-      let divCol = document.createElement("div");
-      divCol.classList.add("col");
+    let divCard = document.createElement("div");
+    divCard.classList.add("card");
+    let image = document.createElement("img");
+    image.src = data[i].Image;
+    image.classList.add("card-img-top", "custom-img");
+    divCard.appendChild(image);
+    //divCard.innerHTML = `<img src="${data[i].Image}" class="card-img-top custom-img" alt="image of a main coon cat" >`
 
-      let divCard = document.createElement("div");
-      divCard.classList.add("card");
-      let image = document.createElement("img");
-      image.src = data[i].Image;
-      image.classList.add("card-img-top", "custom-img");
-      divCard.appendChild(image);
-      //divCard.innerHTML = `<img src="${data[i].Image}" class="card-img-top custom-img" alt="image of a main coon cat" >`
-      
-      let cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
 
-      let cardTop = document.createElement("div");
-      cardTop.classList.add("top-of-card");
+    let cardTop = document.createElement("div");
+    cardTop.classList.add("top-of-card");
 
-      let cardTitle = document.createElement("div");
-      let petName = document.createElement("h5");
-      petName.classList.add("card-title");
-      petName.innerText = data[i].Name;
-      let petBreed = document.createElement("h6");
-      petBreed.classList.add("card-subtitle", "mb-2", "text-body-secondary");
-      petBreed.innerText = data[i].Breed;
+    let cardTitle = document.createElement("div");
+    let petName = document.createElement("h5");
+    petName.classList.add("card-title");
+    petName.innerText = data[i].Name;
+    let petBreed = document.createElement("h6");
+    petBreed.classList.add("card-subtitle", "mb-2", "text-body-secondary");
+    petBreed.innerText = data[i].Breed;
 
-      let genderIcon = document.createElement("div");
-      genderIcon.style = "display: flex";
-      let divSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-      let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-      if (data[i].Sex == 'Female') {
-          
-          divSVG.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-          divSVG.setAttribute("fill", "currentColor");
-          divSVG.setAttribute("class", "icon female bi bi-gender-female");
-          divSVG.setAttribute("viewBox", "0 0 16 16");
+    let genderIcon = document.createElement("div");
+    genderIcon.style = "display: flex";
+    let divSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    if (data[i].Sex == "Female") {
+      divSVG.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+      divSVG.setAttribute("fill", "currentColor");
+      divSVG.setAttribute("class", "icon female bi bi-gender-female");
+      divSVG.setAttribute("viewBox", "0 0 16 16");
 
-          path.setAttribute("fill-rule", "evenodd");
-          path.setAttribute(
-          "d",
-          "M8 1a4 4 0 1 0 0 8 4 4 0 0 0 0-8M3 5a5 5 0 1 1 5.5 4.975V12h2a.5.5 0 0 1 0 1h-2v2.5a.5.5 0 0 1-1 0V13h-2a.5.5 0 0 1 0-1h2V9.975A5 5 0 0 1 3 5"
-          );
-      }
-      else if (data[i].Sex == 'Male') {
-          divSVG.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-          divSVG.setAttribute("fill", "currentColor");
-          divSVG.setAttribute("class", "icon male bi bi-gender-male");
-          divSVG.setAttribute("viewBox", "0 0 16 16");
+      path.setAttribute("fill-rule", "evenodd");
+      path.setAttribute(
+        "d",
+        "M8 1a4 4 0 1 0 0 8 4 4 0 0 0 0-8M3 5a5 5 0 1 1 5.5 4.975V12h2a.5.5 0 0 1 0 1h-2v2.5a.5.5 0 0 1-1 0V13h-2a.5.5 0 0 1 0-1h2V9.975A5 5 0 0 1 3 5"
+      );
+    } else if (data[i].Sex == "Male") {
+      divSVG.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+      divSVG.setAttribute("fill", "currentColor");
+      divSVG.setAttribute("class", "icon male bi bi-gender-male");
+      divSVG.setAttribute("viewBox", "0 0 16 16");
 
-          path.setAttribute("fill-rule", "evenodd");
-          path.setAttribute(
-          "d",
-          "M9.5 2a.5.5 0 0 1 0-1h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-1 0V2.707L9.871 6.836a5 5 0 1 1-.707-.707L13.293 2zM6 6a4 4 0 1 0 0 8 4 4 0 0 0 0-8"
-          );
-      };
-      let petAge = document.createElement("span");
-      petAge.classList.add("age");
-      petAge.innerText = data[i].Age;
-      let cardText = document.createElement("ul");
-      cardText.classList.add("card-text");
-      let info1 = document.createElement("li");
-      info1.innerText = "Color: " + data[i].Color;
-      let info2 = document.createElement("li");
-      if (!data[i].SpayedNeutered) {
-        info2.innerText = 'Not fixed';
+      path.setAttribute("fill-rule", "evenodd");
+      path.setAttribute(
+        "d",
+        "M9.5 2a.5.5 0 0 1 0-1h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-1 0V2.707L9.871 6.836a5 5 0 1 1-.707-.707L13.293 2zM6 6a4 4 0 1 0 0 8 4 4 0 0 0 0-8"
+      );
+    }
+    let petAge = document.createElement("span");
+    petAge.classList.add("age");
+    petAge.innerText = data[i].Age;
+    let cardText = document.createElement("ul");
+    cardText.classList.add("card-text");
+    let info1 = document.createElement("li");
+    info1.innerText = "Color: " + data[i].Color;
+    let info2 = document.createElement("li");
+    if (!data[i].SpayedNeutered) {
+      info2.innerText = "Not fixed";
+    } else {
+      if (data[i].Sex == "Male") {
+        info2.innerText = "Neutered";
       } else {
-        if (data[i].Sex == 'Male') {
-            info2.innerText = 'Neutered';
-        } else {
-            info2.innerText = 'Spayed';
-        };
+        info2.innerText = "Spayed";
+      }
+    }
+
+    divSVG.appendChild(path);
+
+    genderIcon.appendChild(divSVG);
+    genderIcon.appendChild(petAge);
+
+    cardText.append(info1);
+    cardText.append(info2);
+
+    let cardBtn = document.createElement("a");
+    cardBtn.classList.add("btn", "btn-danger", "hidden", "delete-btn");
+    cardBtn.innerText = "Delete";
+    image.addEventListener("click", function () {
+      let modalTitle = document.querySelector("#ModalLabel");
+      let modalContent = document.querySelector("#ModalContent");
+
+      let fixedStatus = data[i].SpayedNeutered; // Used to show Yes or No on the modal for the status of SpayedNeutered
+      if (fixedStatus == true) {
+        // instead of using true or false
+        fixedStatus = "Yes";
+      } else {
+        fixedStatus = "No";
       }
 
-      
-      divSVG.appendChild(path);
-      
-      genderIcon.appendChild(divSVG);
-      genderIcon.appendChild(petAge);
-      
-      cardText.append(info1);
-      cardText.append(info2);
-
-      let cardBtn = document.createElement("a");
-      cardBtn.classList.add("btn", "btn-danger", "hidden", "delete-btn");
-      cardBtn.innerText = "Delete";
-      image.addEventListener("click", function () {
-        let modalTitle = document.querySelector('#ModalLabel');
-        let modalContent = document.querySelector('#ModalContent');
-
-        let fixedStatus = data[i].SpayedNeutered; // Used to show Yes or No on the modal for the status of SpayedNeutered
-        if(fixedStatus == true){                  // instead of using true or false
-          fixedStatus = "Yes";
-        }else{
-          fixedStatus = "No";
-        };
-
-        modalTitle.innerText = data[i].Name + " the " + data[i].Type;
-        modalContent.innerHTML =
-        `
+      modalTitle.innerText = data[i].Name + " the " + data[i].Type;
+      modalContent.innerHTML = `
         <img src="${data[i].Image}" class="modal-img" >
         <dl>
           <dt>A Little Bit About Me:</dt>
@@ -132,29 +129,29 @@ function loadPets() {
           </dd>
         </dl>
         `;
-        let modal = new bootstrap.Modal(document.querySelector('#Modal'));
-        modal.show();
-      })
+      let modal = new bootstrap.Modal(document.querySelector("#Modal"));
+      modal.show();
+    });
 
-      //adding all the divs into the card itself
-      cardTitle.appendChild(petName);
-      cardTitle.appendChild(petBreed);
-      cardTop.appendChild(cardTitle);
-      cardTop.appendChild(genderIcon);
-      cardBody.appendChild(cardTop);
-      cardBody.appendChild(cardText);
-      divCard.appendChild(cardBtn);
-      divCard.appendChild(cardBody);
-      //adding the card into the rows/columns
-      divCol.appendChild(divCard);
-      divRow.appendChild(divCol);
-      console.log(i+1 + " " +data[i].Microchip);
-  };
-  currentIndex += itemsPerPage;
-
-  if (currentIndex >= data.length) {
-    document.querySelector("#loadMoreBtn").style.display = "none";
+    //adding all the divs into the card itself
+    cardTitle.appendChild(petName);
+    cardTitle.appendChild(petBreed);
+    cardTop.appendChild(cardTitle);
+    cardTop.appendChild(genderIcon);
+    cardBody.appendChild(cardTop);
+    cardBody.appendChild(cardText);
+    divCard.appendChild(cardBtn);
+    divCard.appendChild(cardBody);
+    //adding the card into the rows/columns
+    divCol.appendChild(divCard);
+    divRow.appendChild(divCol);
+    console.log(i + 1 + " " + data[i].Microchip);
   }
+  currentIndex += itemsLoaded;
+
+  // if (currentIndex >= data.length) {
+  //   document.querySelector("#loadMoreBtn").style.display = "none";
+  // }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -169,51 +166,40 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log(currentIndex);
 });
 
-$(function() {
-  $(document).on('click', '.delete-btn', function() {
-    $(this).closest('.card').remove();
+$(function () {
+  $(document).on("click", ".delete-btn", function () {
+    $(this).closest(".card").remove();
   });
 });
 
-$(function() {
-  $(document).on('click', '#create-btn', function() {
-        let modal = new bootstrap.Modal(document.querySelector('#CreateModal'));
-        modal.show();
-      });
+$(function () {
+  $(document).on("click", "#create-btn", function () {
+    let modal = new bootstrap.Modal(document.querySelector("#CreateModal"));
+    modal.show();
   });
+});
 
-  
-  $(document).on('click', '.btn-create', function(){
+$(document).on("click", ".btn-create", function () {
+  const formData = {
+    Name: document.getElementById("name").value,
+    Image: document.getElementById("petImage").value,
+    Type: document.querySelector("input[name='animalType']:checked")?.value,
+    Breed: document.getElementById("breed").value,
+    Sex: document.querySelector("input[name='sex']:checked")?.value,
+    Color: document.getElementById("color").value,
+    SpayedNeutered:
+      document.querySelector("input[name='spayedNeutered']:checked")?.value ===
+      "true",
+    Age:
+      document.getElementById("age").value +
+      " " +
+      document.getElementById("yearMonth").value,
+    ID: document.getElementById("ID").value,
+    Microchip: document.getElementById("microchip").value,
+    Status: document.getElementById("status").value,
+    Bio: document.getElementById("bio").value,
+  };
 
-    const formData = {
-      Name: document.getElementById("name").value,
-      Image: document.getElementById("formFileSm").files[0] ? document.getElementById("formFileSm").files[0].name : null,
-      Type: document.querySelector("input[name='animalType']:checked")?.value,
-      Breed: document.getElementById("breed").value,
-      Sex: document.querySelector("input[name='sex']:checked")?.value,
-      Color: document.getElementById("color").value,
-      SpayedNeutered: document.querySelector("input[name='spayedNeutered']:checked")?.value === "true",
-      Age: document.getElementById("age").value + " " + document.getElementById("yearMonth").value,
-      ID: document.getElementById("ID").value,
-      Microchip: document.getElementById("microchip").value,
-      Status: document.getElementById("status").value,
-      Bio: document.getElementById("bio").value,
-    };
-
-    data.push(formData);
-    console.log(data); // Logs updated data array
-    document.getElementById("create").reset();
-    total += 1;
-    console.log("total:" + total);
-    console.log("original:" + original);
-    if (total > original) {
-      console.log("total was larger than original updating")
-      document.querySelector("#loadMoreBtn").style.display = "block";
-      original = total;
-      console.log("total: " + total);
-      console.log("original: " + original);
-    }
-  });
-
-
-  
+  data.push(formData);
+  document.querySelector("#loadMoreBtn").style.display = "block";
+});
